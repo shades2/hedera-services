34c0564d65d18879986bd8f996cb402f154a0fb6
Cody Littley <cody.littley@swirlds.com>
2021-12-06 14:53:41 -0600

    Compare states.

--- hedera-node/src/main/java/com/hedera/services/LoadState.java
+++ hedera-node/src/main/java/com/hedera/services/LoadState.java
@@ -5,12 +5,16 @@ import com.swirlds.common.constructable.ConstructableRegistry;
 import com.swirlds.common.constructable.ConstructableRegistryException;
 import com.swirlds.common.crypto.CryptoFactory;
 import com.swirlds.common.crypto.Hash;
+import com.swirlds.common.merkle.MerkleNode;
+import com.swirlds.common.merkle.route.MerkleRoute;
 import com.swirlds.platform.SignedStateFileManager;
 import com.swirlds.platform.state.SignedState;
 import com.swirlds.platform.state.State;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoadState {
 
@@ -43,6 +47,29 @@ public class LoadState {
         }
     }
 
+    private static void compareStates(final State state1, final State state2) {
+
+        final Map<MerkleRoute, Hash> state1Hashes = new HashMap<>();
+
+        state1.forEachNode((final MerkleNode node) -> {
+            if (node != null) {
+                state1Hashes.put(node.getRoute(), node.getHash());
+            }
+        });
+
+        state2.forEachNode((final MerkleNode node2) -> {
+
+            if (node2 != null) {
+                final Hash node1Hash = state1Hashes.get(node2.getRoute());
+
+                if (!node2.getHash().equals(node1Hash)) {
+                    System.out.println("differing hashes, node type = " +
+                            node2.getClass().getSimpleName() + " @ " + node2.getRoute());
+                }
+            }
+        });
+    }
+
     public static void main(final String[] args) {
 
         try {
@@ -54,12 +81,16 @@ public class LoadState {
                 return;
             }
 
-            final File state1 = new File(args[0]);
-            final File state2 = new File(args[1]);
+            final File file1 = new File(args[0]);
+            final File file2 = new File(args[1]);
 
-            System.out.println("comparing " + state1 + " with " + state2);
+            System.out.println("comparing " + file1 + " with " + file2);
 
-            loadState(state1);
+            final State state1 = loadState(file1);
+            final State state2 = loadState(file2);
+
+            compareStates(state1, state2);
+
         } catch (final Exception ex) {
             ex.printStackTrace();
         }
