84c25896ccecebccf1b4c85e8bc7572b7ec17b56
Cody Littley <cody.littley@swirlds.com>
2022-01-06 16:01:58 -0600

    Fixed nullpointer.

--- hedera-node/src/main/java/com/hedera/services/LoadState.java
+++ hedera-node/src/main/java/com/hedera/services/LoadState.java
@@ -7,142 +7,140 @@ import com.swirlds.common.crypto.CryptoFactory;
 import com.swirlds.common.crypto.Hash;
 import com.swirlds.common.merkle.MerkleNode;
 import com.swirlds.common.merkle.iterators.MerkleDepthFirstIterator;
-import com.swirlds.common.merkle.route.MerkleRoute;
 import com.swirlds.platform.SignedStateFileManager;
 import com.swirlds.platform.state.SignedState;
 import com.swirlds.platform.state.State;
 import org.apache.commons.lang3.tuple.Pair;
-import org.checkerframework.checker.units.qual.A;
-import org.springframework.util.CompositeIterator;
 
 import java.io.File;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class LoadState {
 
-    private static void registerConstructables() {
-        try {
+	private static void registerConstructables() {
+		try {
 
-            DbManager.getInstance(true);
+			DbManager.getInstance(true);
 
-            ConstructableRegistry.registerConstructables("com.swirlds");
-            ConstructableRegistry.registerConstructables("com.hedera");
+			ConstructableRegistry.registerConstructables("com.swirlds");
+			ConstructableRegistry.registerConstructables("com.hedera");
 
-        } catch (final ConstructableRegistryException ex) {
-            throw new RuntimeException(ex);
-        }
-    }
+		} catch (final ConstructableRegistryException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
 
-    private static State loadState(final File stateFile) {
+	private static State loadState(final File stateFile) {
 
-        try {
-            System.out.println("loading state @ " + stateFile);
-            final Pair<Hash, SignedState> pair = SignedStateFileManager.readSignedStateFromFile(stateFile);
-            final State state = pair.getRight().getState();
-            System.out.println("hashing state @ " + stateFile);
-            CryptoFactory.getInstance().digestTreeAsync(state).get();
-            return state;
+		try {
+			System.out.println("loading state @ " + stateFile);
+			final Pair<Hash, SignedState> pair = SignedStateFileManager.readSignedStateFromFile(stateFile);
+			final State state = pair.getRight().getState();
+			System.out.println("hashing state @ " + stateFile);
+			CryptoFactory.getInstance().digestTreeAsync(state).get();
+			return state;
 
-        } catch (final Exception ex) {
-            throw new RuntimeException(ex);
-        }
-    }
+		} catch (final Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
 
-    private static class ComparisonIterator extends MerkleDepthFirstIterator<MerkleNode, MerkleNode> {
+	private static class ComparisonIterator extends MerkleDepthFirstIterator<MerkleNode, MerkleNode> {
 
-        private final MerkleNode rootB;
+		private final MerkleNode rootB;
 
-        public ComparisonIterator(final MerkleNode rootA, final MerkleNode rootB) {
-            super(rootA);
-            this.rootB = rootB;
-        }
+		public ComparisonIterator(final MerkleNode rootA, final MerkleNode rootB) {
+			super(rootA);
+			this.rootB = rootB;
+		}
 
-        @Override
-        protected boolean shouldNodeBeVisited(MerkleNode nodeA) {
+		@Override
+		protected boolean shouldNodeBeVisited(MerkleNode nodeA) {
 
-            if (nodeA == null) {
-                return false;
-            }
+			if (nodeA == null) {
+				return false;
+			}
 
-            MerkleNode nodeB = null;
-            try {
-                nodeB = rootB.getNodeAtRoute(nodeA.getRoute());
-            } catch (final Exception ignored) {
+			MerkleNode nodeB = null;
+			try {
+				nodeB = rootB.getNodeAtRoute(nodeA.getRoute());
+			} catch (final Exception ignored) {
 
-            }
+			}
 
-            if (nodeB == null) {
-                return true;
-            }
+			if (nodeB == null) {
+				return true;
+			}
 
-            final Hash hashA = nodeA.getHash();
-            final Hash hashB = nodeB.getHash();
+			final Hash hashA = nodeA.getHash();
+			final Hash hashB = nodeB.getHash();
 
-            return !hashA.equals(hashB);
-        }
-    }
+			return !hashA.equals(hashB);
+		}
+	}
 
-    private static void compareStates(final State stateA, final State stateB) {
-        final Iterator<MerkleNode> iterator = new ComparisonIterator(stateA, stateB);
+	private static void compareStates(final State stateA, final State stateB) {
+		final Iterator<MerkleNode> iterator = new ComparisonIterator(stateA, stateB);
 
-        iterator.forEachRemaining((final MerkleNode nodeA) -> {
-            MerkleNode nodeB = null;
-            try {
-                nodeB = stateB.getNodeAtRoute(nodeA.getRoute());
-            } catch (final Exception ignored) {
+		iterator.forEachRemaining((final MerkleNode nodeA) -> {
+			MerkleNode nodeB = null;
+			try {
+				nodeB = stateB.getNodeAtRoute(nodeA.getRoute());
+			} catch (final Exception ignored) {
 
-            }
+			}
 
-            if (nodeB == null) {
-                System.out.println("state 2 does not have that corresponds to " + nodeA.getClass().getSimpleName()
-                        + " @ " + nodeA.getRoute());
-                return;
-            }
+			if (nodeB == null) {
 
-            if (nodeA.getClassId() != nodeB.getClassId()) {
-                System.out.println("mismatched class IDs @ " + nodeA.getRoute() + ": " +
-                        nodeA.getClass().getSimpleName() + " vs " + nodeB.getClass().getSimpleName());
-                return;
-            }
+				final String classA = nodeA == null ? "NULL" : nodeA.getClass().getSimpleName();
+				final String classB = nodeB == null ? "NULL" : nodeB.getClass().getSimpleName();
 
-            if (!nodeA.getHash().equals(nodeB.getHash())) {
-                System.out.println("mismatched hashes @ " + nodeA.getRoute() + " for type " +
-                        nodeA.getClass().getSimpleName());
-            }
-        });
+				System.out.println("Mismatched types. Type A = " + classA + ", Type B = " +
+						classB + " @ " + nodeA.getRoute());
+				return;
+			}
 
-    }
+			if (nodeA.getClassId() != nodeB.getClassId()) {
+				System.out.println("mismatched class IDs @ " + nodeA.getRoute() + ": " +
+						nodeA.getClass().getSimpleName() + " vs " + nodeB.getClass().getSimpleName());
+				return;
+			}
 
-    public static void main(final String[] args) {
+			if (!nodeA.getHash().equals(nodeB.getHash())) {
+				System.out.println("mismatched hashes @ " + nodeA.getRoute() + " for type " +
+						nodeA.getClass().getSimpleName());
+			}
+		});
 
-        try {
+	}
 
-            registerConstructables();
+	public static void main(final String[] args) {
 
-            if (args.length != 2) {
-                System.err.println("Invalid number of arguments, 2 arguments expected (states to compare)");
-                return;
-            }
+		try {
 
-            final File fileA = new File(args[0]);
-            final File fileB = new File(args[1]);
+			registerConstructables();
 
-            System.out.println("comparing " + fileA + " with " + fileB);
+			if (args.length != 2) {
+				System.err.println("Invalid number of arguments, 2 arguments expected (states to compare)");
+				return;
+			}
 
-            final State stateA = loadState(fileA);
-            final State stateB = loadState(fileB);
+			final File fileA = new File(args[0]);
+			final File fileB = new File(args[1]);
 
-            System.out.println("Hash A: " + stateA.getHash());
-            System.out.println("Hash B: " + stateB.getHash());
+			System.out.println("comparing " + fileA + " with " + fileB);
 
-            compareStates(stateA, stateB);
+			final State stateA = loadState(fileA);
+			final State stateB = loadState(fileB);
 
-        } catch (final Exception ex) {
-            ex.printStackTrace();
-        }
-    }
+			System.out.println("Hash A: " + stateA.getHash());
+			System.out.println("Hash B: " + stateB.getHash());
+
+			compareStates(stateA, stateB);
+
+		} catch (final Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 
 }
